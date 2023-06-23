a25099aabf1646a90b52b0c92b633511e8a2c8af
Xuan Hoan <mbxhoan001@gmail.com>
2023-06-23 14:41:39 +0700

    update

--- spec.js
+++ spec.js
@@ -17,6 +17,93 @@ var spec =
     ],
     schemes: ["https"],    // Sử dụng scheme gì? HTTP, HTTPS?
     paths: {
+        "/get-data-client?event_code={eventCode}&order_id={orderId}": {    // Đường dẫn. Kết hợp với host và basePath sẽ thành localhost:3000/api/v1/admin/
+            get: {        // Phương thức gửi request: get, post, put, delete
+                tags: ["client"],
+                summary: "Lấy dữ liệu khách hàng",
+                description: "Lấy dữ liệu khách hàng",
+                operationId: "getDataClient",
+                consumes: ["multipart/form-data"],    // Loại dữ liệu gửi đi
+                produces: ["application/json"],       // Loại dữ liệu trả về
+                parameters: [  
+                    {
+                        "in": "query",
+                        "name": "event_code",
+                        "description": "Mã sự kiện",
+                        "required": "true",
+                        "schema": {
+                            "type": "string",
+                            "example": "TANDAIPHUC2023"
+                        },
+                    },
+                    {
+                        "in": "query",
+                        "name": "order_id",
+                        "description": "Mã đơn hàng",
+                        "required": "true",
+                        "schema": {
+                            "type": "string",
+                            "example": "123"
+                        },
+                    },     
+                    {
+                        "in": "header",
+                        "name": "Accept",
+                        "required": "true",
+                        "schema": {
+                            "type": "string",
+                            "example": "application/json"
+                        },
+                    },   
+                    {
+                        "in": "header",
+                        "name": "App-Key",
+                        "description": "App Key (= API Key)",
+                        "required": "true",
+                        "schema": {
+                            "type": "string",
+                        },
+                    },      
+                    {
+                        "in": "header",
+                        "name": "User-Agent",
+                        "description": "User",
+                        "required": "true",
+                        "schema": {
+                            "type": "string",
+                            "example": "PDA"
+                        },
+                    },
+                    {
+                        "in": "header",
+                        "name": "Secret-Token",
+                        "description": "Token để access",
+                        "required": "true",
+                        "schema": {
+                            "type": "string",
+                            "example": "KMfnSD4Qgtze5OODxxx"
+                        },
+                    },
+                ],
+                responses: {
+                    200: {
+                        description: "Thông tin khách mời tham dự sự kiện",
+                        schema: {
+                            $ref: "#/definitions/ApiResponse200_get" 
+                        }
+                    },
+                    400: {
+                        description: "Sai thông tin trường, Sai secret token, IP không hợp lệ, Lỗi cấu hình,...",
+                        schema: {
+                            $ref: "#/definitions/ApiResponse400_get"  
+                        }
+                    }
+                },
+                security: [
+                    
+                ]
+            }
+        },
         "/post-data-client": {    // Đường dẫn. Kết hợp với host và basePath sẽ thành localhost:3000/api/v1/admin/
             post: {        // Phương thức gửi request: get, post, put, delete
                 tags: ["client"],
@@ -77,19 +164,19 @@ var spec =
                     200: {
                         description: "Cập nhật thông tin khách hành thành công",
                         schema: {
-                            $ref: "#/definitions/ApiResponse200" 
+                            $ref: "#/definitions/ApiResponse200_post" 
                         }
                     },
                     400: {
                         description: "Sai thông tin trường, Sai secret token, IP không hợp lệ, Lỗi cấu hình,...",
                         schema: {
-                            $ref: "#/definitions/ApiResponse400"  
+                            $ref: "#/definitions/ApiResponse400_post"  
                         }
                     },
                     422: {
                         description: "Thiếu thông tin của trường quan trọng (securiry_token, event_code, name, email)",
                         schema: {
-                            $ref: "#/definitions/ApiResponse422"  
+                            $ref: "#/definitions/ApiResponse422_post"  
                         }
                     },
                 },
@@ -153,7 +240,104 @@ var spec =
                 
             }
         },
-        ApiResponse200: {          
+        ApiResponse200_get: {          
+            type: "object",       
+            properties: {      
+                status: {
+                    example: "success"   
+                },
+                status_code: {
+                    example: "200"
+                },
+                message: {    
+                    example: "Thông tin khách mời tham dự sự kiện"
+                },
+                data: {
+                    type: "object",
+                    properties: {
+                        id: {
+                            type: "number",
+                            example: 4604
+                        },
+                        qrcode: {
+                            type: "string",
+                            example: "TANDAIPHUC2023050623023542B9KGT" 
+                        },
+                        ref_id: {
+                            type: "number",
+                            example: 43993 
+                        },
+                        name: {
+                            type: "string",
+                            example: "Nguyễn Trần Nam" 
+                        },
+                        phone: {           
+                            type: "number",   
+                            example: "0902639xxx"
+                        },
+                        email: {
+                            type: "string",
+                            example: "ntn@gmail.com"   
+                        },
+                        type: {
+                            type: "string",
+                            example: "NORMAL"   
+                        },
+                        status: {
+                            type: "string",
+                            example: "NEW"   
+                        },
+                        img_qrcode: {
+                            type: "string",
+                            example: ""   
+                        },
+                        document_pdf: {
+                            type: "string",
+                            example: ""   
+                        },
+                        avatar: {
+                            type: "string",
+                            example: ""   
+                        },
+                        checkin_count: {
+                            type: "number",
+                            example: 0 
+                        },
+                        custom_fields: {
+                            type: "object",
+                            example: {
+                                "address": "HCM, Viet Nam",
+                                "order_time": "07/10/2001 13:52:09",
+                                "payment_time": "07/10/2001 14:00:25",
+                                "transaction_id": 14029115,
+                                "ticket_conference": "Session A\n",
+                                "ticket_early": "Member (Hội viên thuộc các LCH) – 1.500.000\n",
+                                "ticket_standard": "Member (Hội viên thuộc các LCH) – 1.500.000\n"
+                            }
+                        }
+                    }    
+                },
+            }
+        },     
+        ApiResponse400_get: {          
+            type: "object",       
+            properties: {      
+                status: {
+                    example: "error"   
+                },
+                status_code: {
+                    example: "400"
+                },
+                message: {    
+                    example: "Không tìm thấy thông tin khách mời."
+                },
+                data: {
+                    type: "object",
+                    example: null 
+                },
+            }
+        },             
+        ApiResponse200_post: {          
             type: "object",       
             properties: {      
                 status: {
@@ -232,7 +416,7 @@ var spec =
                 },
             }
         },           
-        ApiResponse400: {          
+        ApiResponse400_post: {          
             type: "object",       
             properties: {      
                 status: {
@@ -250,7 +434,7 @@ var spec =
                 },
             }
         },           
-        ApiResponse422: {          
+        ApiResponse422_post: {          
             type: "object",       
             properties: {      
                 status: {
